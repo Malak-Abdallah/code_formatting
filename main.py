a8b30596ac6d1b9dee0020450b1ee593b9b5b1c3
Malk Abdallah <malkobeidallah@gmail.com>
2021-08-25 10:13:06 +0300

    edited yaml file by adding new hooks

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 # pre-commit hook : run different hooks to your code and check the code format
 # if the hook passed we can commit the code, else we have to fix the code and recheck
-
 # git init to the code we want to pre-commit
 # then pre-commit install
 # create .pre-commit-config.yaml file to add the hooks we want to check
-
 # ----------------------------------------
 # naming conventions:
 
@@ -17,14 +15,14 @@ def multiply_by_two(x):  # recommended
 class Formatting:
     # choose clear names for variables or functions
     # variables and function must be small letters  and use underscore if more than one word is needed
-    x = "John Smith"  # Not recommended
-    name = "John Smith"  # recommended
+    x = 'John Smith'  # Not recommended
+    name = 'John Smith'  # recommended
 
     def db(self, x):  # Not recommended
         return x * 2
 
     #  CONSTANTS must be in capital letters
-    NAME = "malk"
+    NAME = 'malk'
 
     # -----------------------------------
     # Linters are programs that analyze code and flag errors, then provide suggestion on how to fix the error
@@ -32,5 +30,19 @@ class Formatting:
     # there are autoformatters as black , than can be run in the terminal
 
 
-str = "This article is written in {}"
-print(str.format("Python"))
+str = 'This article is written in {}'
+print(str.format('Python'))
+
+
+class MyName:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def print_info(self):
+        print(f'hi my name is {self.first_name} {self.last_name}')
+
+
+name = MyName('malak ', 'abdallh')
+
+name.print_info()
